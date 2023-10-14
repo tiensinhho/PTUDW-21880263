6b3a6a6befea2c13b56a5c60177232dcb8abe27b
Sinh Ho <sinh.ho@hotmail.com>
2023-10-14 11:24:35 +0700

    update

--- index.js
+++ index.js
@@ -2,14 +2,29 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5050;
+const expressHandlebars = require('express-handlebars');
 //cau hinh public static folder
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) =>{
-    res.send('Hello to Eshop')
+//cau hinh su dung express-handlebars
+app.engine('hbs', expressHandlebars.engine({
+    layoutsDir: __dirname+ '/views/layouts',
+    partialsDir: __dirname+ '/views/partials',
+    extname: 'hbs',
+    defaultLayout: 'layout'
+}) );
+app.set('view engine', 'hbs');
+
+// //routes
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.get('/:page',(req, res) =>{
+    res.render(req.params.page);
 })
 
 //khoi dong web server
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`sever is running on port ${port}`);
 })
